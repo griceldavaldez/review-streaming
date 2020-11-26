c1df8269aced20ba2e6c854eebaf74dd27353b15
griceldavaldez <gricel_valdez99@hotmail.com>
2020-11-25 22:55:01 -0300

    Implementación de utilidades

--- src/main/java/com/review/utils/SortUtils.java
+++ src/main/java/com/review/utils/SortUtils.java
@@ -5,16 +5,21 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import com.review.bean.*;
+import com.review.bean.Administrador;
+import com.review.bean.Pelicula;
+import com.review.bean.SitioReview;
+import com.review.bean.Usuario;
+
+//import com.review.bean.*;
 
 public final class SortUtils {
 	
 	public enum SortMode {ASC, DESC}
 
-	public static void sortUsuarioPorNombre(final SortMode sortMode, List<Usuario> list) {
+	public static void sortPorNombre(final SortMode sortMode, List<Ordenable> list) {
 		
-		Collections.sort(list, new Comparator<Usuario>(){
-		    public int compare(Usuario o1, Usuario o2) {
+		Collections.sort(list, new Comparator<Ordenable>(){
+		    public int compare(Ordenable o1, Ordenable o2) {
 		    	if(SortMode.DESC.equals(sortMode)) {
 		    		return o2.getNombre().compareToIgnoreCase(o1.getNombre());
 		    	}
@@ -23,8 +28,18 @@ public final class SortUtils {
 		});
 	}
 	
+	public static void imprimirListaOrdenada(List<Ordenable> list) {
+		System.out.println("\nLista ordenada\n");
+		for(Ordenable a : list) {
+			System.out.println(a.getNombre());
+		}
+	}
+	
+//PARA PROBAR EL CODIGO, FUNCIONA DESDE CUALQUIER OTRA CLASE TAMBIEN 	
 	public static void main(String[] args) {
-		List<Usuario> list = new ArrayList<Usuario>();
+		
+		//para ordenar usuarios 
+		List<Ordenable> list = new ArrayList<Ordenable>();
 		Usuario u = new Administrador();
 		u.setNombre("Carlos");
 		Usuario u2 = new Administrador();
@@ -34,11 +49,37 @@ public final class SortUtils {
 		list.add(u);
 		list.add(u2);
 		list.add(u3);
+		SortUtils.sortPorNombre(SortMode.DESC,list);
+		SortUtils.imprimirListaOrdenada(list);
 		
-		SortUtils.sortUsuarioPorNombre(SortMode.DESC,list);
-
-		for(Usuario a : list) {
-			System.out.println(a.getNombre());
-		}
-	}
+		//para ordenar sitios de review
+		List<Ordenable> list2 = new ArrayList<Ordenable>();
+		SitioReview sitio1 = new SitioReview();
+		sitio1.setNombre("IMDB");
+		SitioReview sitio2 = new SitioReview();
+		sitio2.setNombre("Rotten Tomatoes");
+		SitioReview sitio3 = new SitioReview();
+		sitio3.setNombre("Film Affinity");
+		list2.add(sitio1);
+		list2.add(sitio2);
+		list2.add(sitio3);
+		SortUtils.sortPorNombre(SortMode.ASC,list2);
+		SortUtils.imprimirListaOrdenada(list2);
+		
+		//para ordenar videos
+		
+		List<Ordenable> list3 = new ArrayList<Ordenable>();
+		Pelicula peli1= new Pelicula();
+		peli1.setTitulo("Titanic");
+		Pelicula peli2= new Pelicula();
+		peli2.setTitulo("El Señor de los Anilllos");
+		Pelicula peli3= new Pelicula();
+		peli3.setTitulo("It");
+		list3.add(peli1);
+		list3.add(peli2);
+		list3.add(peli3);
+		SortUtils.sortPorNombre(SortMode.ASC,list3);
+		SortUtils.imprimirListaOrdenada(list3);
+		
+	} 
 }
